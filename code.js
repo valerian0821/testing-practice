5f637be3b6562622be441e256abf16c5bc0c5a0d
Valerian Ha <ha.valerian@gmail.com>
2025-05-31 18:30:32 -0400

    Add code that passes tests for capitalize

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
-const capitalize = () => {
-
+const capitalize = (str) => {
+  if (!str) {
+    return "";
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
 module.exports = capitalize;
